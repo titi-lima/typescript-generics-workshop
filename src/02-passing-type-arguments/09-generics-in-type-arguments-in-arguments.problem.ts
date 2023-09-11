9ff394d2cd7541dd72a63d4e1ae8ffac433d73e4
titi-lima <titisau@gmail.com>
2023-09-10 23:53:28 -0300

    solved 09

--- src/02-passing-type-arguments/09-generics-in-type-arguments-in-arguments.problem.ts
+++ src/02-passing-type-arguments/09-generics-in-type-arguments-in-arguments.problem.ts
@@ -11,7 +11,7 @@ export class Component<TProps> {
   getProps = () => this.props;
 }
 
-const cloneComponent = (component: unknown) => {
+const cloneComponent = <TProp>(component: Component<TProp>) => {
   return new Component(component.getProps());
 };
 
